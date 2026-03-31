115b57d0e272aad2d898266ca985582350f0afd4
EnrikeLikeGenesis <benjaalcibar@gmail.com>
2026-03-31 20:26:27 -0300

    Dibujo de linea entre dos puntos

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ L.marker([-34.6037, -58.3816])
 map.on('click', function(e) {
    alert("Lat: " + e.latlng.lat + " Lng: " + e.latlng.lng);
 });
+let puntos = [];
+map.on('click', function(e) {
+   puntos.push([e.latlng.lat, e.latlng.lng]);
+
+
+   if (puntos.length === 2) {
+       L.polyline(puntos, { color: 'blue' }).addTo(map);
+   }
+});
