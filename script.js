9cf60a7630371b9d247cda525524d86852dfdd46
EnrikeLikeGenesis <benjaalcibar@gmail.com>
2026-04-30 21:37:25 -0300

    agrego limpiar mapa y capas oscura/clara

--- script.js
+++ script.js
@@ -1,29 +1,78 @@
-var map = L.map('map').setView([-34.6037, -58.3816], 13);
+let mapaNormal = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+let mapaOscuro = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png');
 
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   attribution: 'OpenStreetMap'
-}).addTo(map);
-
-
-
-L.marker([-34.6037, -58.3816])
-   .addTo(map)
-   .bindPopup("Ubicación inicial")
-   .openPopup();
-
-
-map.on('click', function(e) {
-   alert("Lat: " + e.latlng.lat + " Lng: " + e.latlng.lng);
+let map = L.map('map', {
+    center: [-34.6037, -58.3816],
+    zoom: 13,
+    layers: [mapaNormal]
 });
+
+let baseMaps = {
+    "🌍 Normal": mapaNormal,
+    "🌑 Oscuro": mapaOscuro
+};
+
+L.control.layers(baseMaps).addTo(map);
 
 
 let puntos = [];
-map.on('click', function(e) {
-   puntos.push([e.latlng.lat, e.latlng.lng]);
+let figura = null;
+let marcadores = [];
 
+map.on('click', function (e) {
 
-   if (puntos.length === 2) {
-       L.polyline(puntos, { color: 'blue' }).addTo(map);
-   }
+    let latlng = [e.latlng.lat, e.latlng.lng];
+    puntos.push(latlng);
+
+    let marker = L.marker(latlng).addTo(map);
+    marcadores.push(marker);
+
+    if (figura) {
+        map.removeLayer(figura);
+    }
+
+    if (puntos.length === 2) {
+        figura = L.polyline(puntos).addTo(map);
+    }
+
+    if (puntos.length >= 3) {
+        figura = L.polygon(puntos).addTo(map);
+    }
 });
+
+function limpiarMapa() {
+
+    marcadores.forEach(m => map.removeLayer(m));
+    marcadores = [];
+
+    if (figura) {
+        map.removeLayer(figura);
+        figura = null;
+    }
+
+    puntos = [];
+}
+
+
+
+// calcular la distancia
+let puntosDistancia = [];
+
+map.on('click', function(e) {
+
+    // guardar punto
+    puntosDistancia.push(e.latlng);
+
+    // si hay 2 puntos → calcular
+    if (puntosDistancia.length === 2) {
+
+        let distancia = puntosDistancia[0].distanceTo(puntosDistancia[1]);
+
+        alert("Distancia: " + distancia.toFixed(2) + " metros");
+
+        // resetear para volver a medir
+        puntosDistancia = [];
+    }
+
+});
+
